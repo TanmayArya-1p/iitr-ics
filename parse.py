534304d361e524228f5c916437c0c64a720f70b1
Tanmay Arya <tanmayarya2018@gmail.com>
2026-01-10 15:46:45 +0530

    add subject name parsing

--- parse.py
+++ parse.py
@@ -9,7 +9,7 @@ class ICSCalendar:
 		self.calendar = self.__produce_calendar(response_json)
 
 
-	def __extract_events(self,response_json):
+	def __extract_events(self, response_json, course_name_lookup):
 		res = []
 		for i in response_json["result"]:
 			type = HUMAN_SLOT_TYPE[i["Slot_Type"]]
@@ -31,21 +31,38 @@ class ICSCalendar:
 			start_t = parse_to_arrow(start_t_str, day_code)
 			end_t = parse_to_arrow(end_t_str, day_code)
 
+            # recur for next 6 months
+			until_date = start_t.shift(months=6).replace(hour=23, minute=59, second=59)
+
 			e = Event()
 			e.name = event_name
 			e.begin = start_t
 			e.end = end_t
+			e.description = course_name_lookup.get(course, "")
+
 			rrule = Container("RRULE")
 			rrule.append(ContentLine(name="FREQ", value="WEEKLY"))
 			rrule.append(ContentLine(name="BYDAY", value=day_code))
+			rrule.append(ContentLine(name="UNTIL", value=until_date.strftime("%Y%m%dT%H%M%SZ")))
 
 			e.extra.append(rrule)
 			res.append(e)
 		return res
 
+
+	def __extract_course_names(self, response_json):
+		m = {}
+		for i in response_json["CourseList"]:
+			try:
+				m[i["SubjectCode"]] = i["SubjectName"]
+			except:
+				pass
+		return m
+
 	def __produce_calendar(self,response_json):
 		c = Calendar()
-		events = self.__extract_events(response_json)
+		course_name_lookup = self.__extract_course_names(response_json)
+		events = self.__extract_events(response_json, course_name_lookup)
 		for ev in events:
 			c.events.add(ev)
 
